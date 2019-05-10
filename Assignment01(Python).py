9b4484ff4331f240dcb59fd615749e706cd149e5
MuhammadNaeem <muhammadnaeem93@hotmail.com>
2019-05-11 02:41:09 +0500

    Completed

---
+++ Assignment01(Python).py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ASSIGNMENT 
+
+# <h3> 1 : Declare and initialize a variable name total_marks with a value 500</h3>
+# 
+
+# In[11]:
+
+
+Total_Marks = 500
+
+print("Total Marks ->", Total_Marks)
+
+
+# <h3> 2 : Declare and initialize 5 variables name maths , english , urdu , physics and chemisrty with different integer values</h3>
+
+# In[4]:
+
+
+Maths = 95
+English = 85
+Urdu = 70
+Physics = 80
+Chemistry = 75
+
+
+# <h3>3 : Calculate the obtained marks And assign the value In variable name obtained_marks</h3>
+
+# In[13]:
+
+
+Obtained_Marks = Maths + English + Urdu + Physics + Chemistry
+
+print("Marks Obtained ->" , Obtained_Marks)
+
+
+# <h3>4 : Calculate percentage and assign the value in variable name percentage</h3>
+
+# In[16]:
+
+
+Percentage = (Obtained_Marks/Total_Marks)*100
+
+print("Percentage ->", Percentage,'%')
+
+
+# <h3>5 : Declare and initialize an empty variable name grade </h3>
+
+# In[20]:
+
+
+Grade = None
+
+
+# ### 6: print your biodata 
+
+# In[24]:
+
+
+print("Name: Muhammad Naeem Ahmed")
+print("Gender: Male")
+print("Religion: Islam")
+print("Nationality: Pakistani")
+print("Residence: Karachi,Pakistan")
+print("Maritial Status: Single")
+
